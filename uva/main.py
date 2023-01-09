e3fb947d6600eaeef33345490fd35dbc72e27904
aleksamarkoni <aleksamarkoni@gmail.com>
2023-01-09 02:13:36 +0100

    Udate help for submiting

--- uva/main.py
+++ uva/main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import typer
 from rich import print
 
@@ -9,7 +11,7 @@ app = typer.Typer()
 @app.command()
 def login(
     username: str = typer.Option(
-        ...,
+        None,
         "--username",
         "-u",
         prompt="Enter your uva username",
@@ -17,7 +19,7 @@ def login(
         help="Your uva username"
     ),
     password: str = typer.Option(
-        ...,
+        None,
         "--password",
         "-p",
         prompt="Enter your uva password",
@@ -38,16 +40,37 @@ def logout():
 
 
 @app.command()
+def submit(
+        problem_id: int = typer.Option(
+          ...,
+          "--problem-id",
+          "-id",
+          show_default=False,
+          help="Uva problem id"
+        ),
+        filepath: str = typer.Option(
+            ...,
+            "--filepath",
+            "-f",
+            show_default=False,
+            help="Path to the solution file"
+        ),
+        language: int = typer.Option(
+            None,
+            "--language",
+            "-l",
+            show_default=False,
+            help="1 for ANSI, 2 for JAVA, 3 for C++, 4 for Pascal, 5 for C++11, 6 for Python."
+        )
+):
+    commands.submit(problem_id, filepath, language)
+
+
+@app.command()
 def latest_subs(count: int = 10):
     subs = commands.get_latest_subs(count)
     if subs:
         print(subs)
-
-
-# 1 for ANSI, 2 for JAVA, 3 for C++, 4 for Pascal, 5 for C++11, 6 for Python.
-@app.command()
-def submit(problem_id: int, filepath: str, language: int):
-    commands.submit(problem_id, filepath, language)
 
 
 @app.command()
